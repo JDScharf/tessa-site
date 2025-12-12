bb1c28c35488219c3e653a1670eeba9612dd06b7
Jason Scharf <jasonscharf3@gmail.com>
2025-12-11 23:27:06 -0500

    Removed cards on page.  Added text under images.  Also change the text on mobile, and the backgrond color behind the registry images.

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,15 +10,24 @@ export default function SlaveryAbolitionPage() {
 
           <div className="content-grid">
             {/* Plantation Image */}
-            <div className="image-card">
+            <div>
               <img
+                className="home-images"
                 src="images/west_india_cutting_canes.jpg"
                 alt="Historic plantation scene"
               />
+              <a href="https://search.library.yale.edu/catalog/9945152293408651">
+                <i>
+                  Bridgens, Richard, West India scenery with illustrations of
+                  Negro character, the process of making sugar. from sketches
+                  taken during a voyage to, and residence of seven years in, the
+                  island of Trinidad, 1836?
+                </i>
+              </a>
             </div>
 
             {/* Right Column Text */}
-            <div className="left-text">
+            <div className="home-top-text">
               <p>
                 Slavery in the age of abolition highlights the realities of
                 slavery in some of the last Caribbean islands to be acquired by
@@ -55,39 +64,46 @@ export default function SlaveryAbolitionPage() {
 
           <div className="content-grid">
             {/* Left Column Text */}
-            <div className="left-text">
+            <div className="home-bottom-text">
               <p>
                 Great Britain acquired the former Spanish colony of Trinidad at
                 the 1803 Peace of Amiens, and the former French colony of St.
                 Lucia at the 1814 Treaty of Paris. Britain ruled the islands as
                 Crown Colonies, meaning the colonies were denied the legislative
                 assemblies allowed to British Caribbean colonies established
-                earlier, such as Barbados or Jamaica.</p><br></br>
-                <p> This meant that
-                legislation for Trinidad and St. Lucia was made through
-                metropolitan Orders in Council, limiting local planter influence
-                on the law. As a result, St. Lucia and especially Trinidad
-                became sites where colonial laws later applied throughout the
-                British West Indies, such as registration and later
-                amelioration, was first applied and experimented with.</p> <br></br>
-                
-                <p> Registry
-                books for these Crown colonies are therefore much more detailed
-                than those for islands where the local assembly dictated the
-                form and content of the registries, giving us richer insight on
-                the lives of people enslaved on the expanding fringes of the
-                nineteenth century British Empire.
+                earlier, such as Barbados or Jamaica.
+              </p>
+              <br></br>
+              <p>
+                {" "}
+                This meant that legislation for Trinidad and St. Lucia was made
+                through metropolitan Orders in Council, limiting local planter
+                influence on the law. As a result, St. Lucia and especially
+                Trinidad became sites where colonial laws later applied
+                throughout the British West Indies, such as registration and
+                later amelioration, was first applied and experimented with.
+              </p>{" "}
+              <br></br>
+              <p>
+                {" "}
+                Registry books for these Crown colonies are therefore much more
+                detailed than those for islands where the local assembly
+                dictated the form and content of the registries, giving us
+                richer insight on the lives of people enslaved on the expanding
+                fringes of the nineteenth century British Empire.
               </p>
             </div>
 
             {/* Map */}
-            <div className="image-card">
-              <div className="map-container">
-                <img
-                  src="/images/test_map.jpg"
-                  alt="Historic plantation scene"
-                />
-              </div>
+            <div>
+              <img
+                className="home-images"
+                src="/images/test_map.jpg"
+                alt="Map of Carribean"
+              />
+              <p>
+                <i>Map of the British Caribbean.</i>
+              </p>
             </div>
           </div>
         </div>
@@ -95,45 +111,54 @@ export default function SlaveryAbolitionPage() {
 
       {/* Second Section */}
       <section className="registry-section">
-        <div className="container">
+        <div className="home-title-div">
           <h2>Source Material</h2>
-
+        </div>
+        <div className="container">
           <div className="registry-grid">
-              <img
-                src="images/registry_1.jpg"
-                alt="An old book that is closed from the top."
-                className="registry-image"
-              />
+            <img
+              src="images/registry_1.jpg"
+              alt="An old book that is closed from the top."
+              className="registry-image"
+            />
 
-              <img
-                src="images/registry_2.jpg"
-                alt="An old book that is open from the top."
-                className="registry-image"
-              />
+            <img
+              src="images/registry_2.jpg"
+              alt="An old book that is open from the top."
+              className="registry-image"
+            />
 
-              <img
-                src="images/registry_3.jpg"
-                alt="An old book that is closed from the side."
-                max-width="100%"
-                className="registry-image"
-              />
+            <img
+              src="images/registry_3.jpg"
+              alt="An old book that is closed from the side."
+              max-width="100%"
+              className="registry-image"
+            />
           </div>
 
           <div className="left-text">
             <p>
-Handwritten registries, copies of which are now held in the T71 series of the National Archives 
-of the United Kingdom (TNA), yield detailed information about every individual enslaved in 
-Trinidad and St. Lucia. Following an 1813 Order in Council, enslavers were required to report 
-the first name, last name, “color,” employment, age, stature, country of origin, “marks,” or 
-distinguishing characteristics, and familial relations of each person enslaved in Trinidad; in 1815, 
-they did the same for St. Lucia.</p><br></br> 
-<p>In each island, Registrars and their clerks hand-copied this 
-information into one of two large books, each of which runs to as many as 800 pages: the 
-Registry of Plantation Slaves (i.e., people enslaved on agricultural estates) or the Registry of 
-Personal Slaves (i.e., people enslaved in more urban settings). In 1819, copies of the Registry 
-Books were sent to England, and in 1851 they were transferred to the Public Record Office (now 
-TNA). The registries can be freely accessed at TNA. All information made available on this site 
-is based on human-generated transcriptions of photographs of these original archival documents.  
+              Handwritten registries, copies of which are now held in the T71
+              series of the National Archives of the United Kingdom (TNA), yield
+              detailed information about every individual enslaved in Trinidad
+              and St. Lucia. Following an 1813 Order in Council, enslavers were
+              required to report the first name, last name, “color,” employment,
+              age, stature, country of origin, “marks,” or distinguishing
+              characteristics, and familial relations of each person enslaved in
+              Trinidad; in 1815, they did the same for St. Lucia.
+            </p>
+            <br></br>
+            <p>
+              In each island, Registrars and their clerks hand-copied this
+              information into one of two large books, each of which runs to as
+              many as 800 pages: the Registry of Plantation Slaves (i.e., people
+              enslaved on agricultural estates) or the Registry of Personal
+              Slaves (i.e., people enslaved in more urban settings). In 1819,
+              copies of the Registry Books were sent to England, and in 1851
+              they were transferred to the Public Record Office (now TNA). The
+              registries can be freely accessed at TNA. All information made
+              available on this site is based on human-generated transcriptions
+              of photographs of these original archival documents.
             </p>
           </div>
         </div>
