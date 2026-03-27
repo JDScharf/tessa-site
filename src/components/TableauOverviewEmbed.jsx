d5a4a7fb0ac49b1806852a13365577f690c17694
Jason Scharf <jasonscharf3@gmail.com>
2026-03-26 20:48:28 -0400

    Tried to fix issue in Tableau overview page that was messing up netlify build.

--- src/components/TableauOverviewEmbed.jsx
+++ src/components/TableauOverviewEmbed.jsx
@@ -38,12 +38,13 @@ function TableauOverviewEmbed() {
       ref={divRef}
     >
       <noscript>
-        <a href="#">
+        <a href="https://public.tableau.com/app/profile/tessa.murphy1162/viz/TMurphyExShcarf/DBbycroptype">
           <img
             alt="DB by crop type"
             src="https://public.tableau.com/static/images/TM/TMurphyExShcarf/DBbycroptype/1_rss.png"
             style={{ border: "none" }}
           />
+          View Dashboard
         </a>
       </noscript>
       <object className="tableauViz" style={{ display: "none" }}>
@@ -69,8 +70,3 @@ function TableauOverviewEmbed() {
 }
 
 export default TableauOverviewEmbed;
-
-// Original Tableau Embed Code
-{
-  /* <div class='tableauPlaceholder' id='viz1774565070991' style='position: relative'><noscript><a href='#'><img alt='DB by crop type ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;TM&#47;TMurphyExShcarf&#47;DBbycroptype&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='TMurphyExShcarf&#47;DBbycroptype' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;TM&#47;TMurphyExShcarf&#47;DBbycroptype&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1774565070991');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='1377px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script> */
-}
